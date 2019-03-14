a33fca537e2fb45f51f478a396966aeecd96dc03
Matt Maneval <mattmaneval@matts-mbp.lan>
2019-03-14 01:04:56 -0400

    preparing site to test view live

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      `gatsby-plugin-offline`,
+      `gatsby-plugin-react-helmet`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
