4315803128979b0125ab05dbc4a18a32580ae8f4
Matt Maneval <mattmaneval@matts-mbp.lan>
2019-03-31 04:44:28 -0400

    Updated meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Poco Piatti`,
+    description: `Perrysburg | Sylvania | Toledo`,
+    author: `Matt Maneval`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
