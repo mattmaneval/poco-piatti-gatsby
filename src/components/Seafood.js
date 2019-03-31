2977b84da93fd7ee55bad3a73748ae78ce16a3ac
Matt Maneval <mattmaneval@matts-mbp.lan>
2019-03-31 16:06:28 -0400

    Re-structured Menu. Made food categories components

---
+++ src/components/Seafood.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Seafood = () => (
+  <div className="Seafood">
+    <h1>Seafood</h1>
+    <span className="Line"></span>
+    <div className="Outline">
+      <h5>Shrimp Saganaki &mdash; Sm 7 Lg 13</h5>
+      <p>Sauteed with tomatoes, garlic, white wine, fresh oregano and feta.</p>
+      <h5>Fried Calamari Zaatar &mdash; Sm 6 Lg 11</h5>
+      <p>Zaatar encrusted and served with spicy marinara.</p>
+      <h5>Shrimp Asti-Spumante &mdash; Sm 7 Lg 13</h5>
+      <p>Sauteed with garlic, asti-spumante and parsley.</p>
+      <h5>Thai Shrimp 'Lasagna' &mdash; Sm 7 Lg 13</h5>
+      <p>Layered with crispy wontons, spicy thai and coconut basil.</p>
+      <h5>Blue Crab Lump Crab Cakes<br /> &mdash; Sm 9 Lg 17</h5>
+      <p>Served with roasted red pepper cream sauce.</p>
+      <h5>Sumac Encrusted Tuna &mdash; Sm 9 Lg 17</h5>
+      <p>Served with tahini and diced cucumber salad.</p>
+    </div>
+  </div>
+);
+
+export default Seafood;
---
+++ src/components/Vegetarian.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Vegetarian = () => (
+  <div className="Seafood">
+    <h1>Vegetarian</h1>
+    <span className="Line"></span>
+      <div className="Outline">
+        <h5>Riad's Hummuos B Tahini<br />&mdash; Sm 5 Lg 9</h5>
+        <p>Served with Pita.</p>
+        <h5>Roasted Garlic Feta Dip &mdash; Sm 6 Lg 9</h5>
+        <p>Served warm with diced tomato.</p>
+        <h5>Lebanee &mdash; Sm 4 Lg 7</h5>
+        <p>Yogurt with cucumber, mint and drizzled with olive oil.</p>
+        <h5>Saganaki Kasseri &mdash; 6</h5>
+        <p>Greek kasseri cheese flamed tableside.</p>
+        <h5>Sauteed Mushrooms &mdash; 8</h5>
+        <h5>Mujadra &mdash; Sm 5 Lg 8</h5>
+        <p>Green lentils and rice with caramelized onions and pickles.</p>
+        <h5>Lubia Bi Zeit &mdash; 8</h5>
+        <p>Sauteed green beans with garlic and onions in a tomato sauce.</p>
+        <h5>Veggie Grape Leaves &mdash; Sm 5 Lg 8</h5>
+        <p>Stuffed with chick peas, tomatoes, rice, onion and lemon.</p>
+        <h5>Poco Brussel Sprouts &mdash; Sm 5 Lg 9</h5>
+        <p>Covered in fig jam, mint yogurt, grapes and toasted pecans.</p>
+        <h5>Roasted Cauliflower &mdash; Sm 5 Lg 9</h5>
+        <p>Tossed in garlic oil, tahini and fresh dill.</p>
+        <h5>Stuffed Portabella Caps &mdash; Sm 6 Lg 10</h5>
+        <p>Mushroom stuffed with a broccoli pesto and topped with pepperjack cheese and tomato.</p>
+      </div>
+  </div>
+);
+
+export default Vegetarian;
