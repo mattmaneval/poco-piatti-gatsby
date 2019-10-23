82f45f637381fc4e20f0833fe86950b32577ba10
Matt Maneval <mattmaneval@matts-mbp.lan>
2019-10-23 14:49:34 -0400

    Removing OnlineOrdering Links until ready.

--- src/components/OnlineOrdering.js
+++ src/components/OnlineOrdering.js
@@ -7,9 +7,7 @@ const OnlineOrdering = () => (
       <div className="OnlineOrderingLinks">
         <button>
           <a href="tel:419-931-0281">Reservations & Carryout <br />419-931-0281</a></button>
-        <button><a href="https://red-fork.com/PocoPiattiPerrysburg">Online Ordering Perrysburg</a></button>
-        <button><a href="https://red-fork.com/PocoPiattiCentral">Online Ordering Sylvania</a></button>
-        <button><a href="https://red-fork.com/PocoPiattiDowntown">Online Ordering Downtown</a></button>
+        
       </div>
     </div>
   </div>
--- src/components/OrdingLinks.js
+++ src/components/OrdingLinks.js
@@ -1,29 +1,3 @@
-<div className="OrderingLinks">
-  <h1>Order Poco online</h1>
-  <span className="Line"></span>
-  <section>
-    <button>
-      <a href="https://red-fork.com/PocoPiattiPerrysburg">
-      <h3>Order now at our<br /> Perrysburg location</h3>
-
-      </a>
-    </button>
-  </section>
-  <section>
-    <button>
-      <a href="https://red-fork.com/PocoPiattiCentral">
-      <h3>Order now at our<br /> Sylvania location</h3>
-
-      </a>
-    </button>
-  </section>
-  <section>
-    <button>
-      <a href="https://red-fork.com/PocoPiattiDowntown
-">
-      <h3>Order now at our<br /> Downtown location</h3>
-
-      </a>
-    </button>
-  </section>
-</div>
+        <button><a href="https://red-fork.com/PocoPiattiPerrysburg">Online Ordering Perrysburg</a></button>
+        <button><a href="https://red-fork.com/PocoPiattiCentral">Online Ordering Sylvania</a></button>
+        <button><a href="https://red-fork.com/PocoPiattiDowntown">Online Ordering Downtown</a></button>
