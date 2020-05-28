a208625d9c6eac0e8d3b8d277dac8283f42471ee
mattmaneval <manevalmusic@gmail.com>
2020-05-28 15:07:45 -0400

    Revert back to original private events

--- src/pages/PrivateEvents.js
+++ src/pages/PrivateEvents.js
@@ -4,43 +4,38 @@ import SEO from "../components/seo"
 import "../styles/_index.scss"
 import PerrysburgRoom from "../components/private-events/PerrysburgRoom"
 import DowntownRoom from "../components/private-events/DowntownRoom"
-import IMG_0049 from "../images/IMG_0049.jpg";
-import bar from "../images/bar.jpg";
-import PrimaryCallout from "../components/home/PrimaryCallout"
 
 const PrivateEvents = () => (
   <Layout>
     <SEO title="Private Events" />
     <div className="PrivateEvents">
-      <div className="wrap">
+      <div className="PrivateEventsWrap">
         <div className="PrivateEventsWrapHeader">
-          <h1>Private Events</h1>
-          <h3>Book your next private party or event at Poco Piatti.
+          <h1>Book your next private party or event at Poco Piatti.
           We offer flexible packages and personal attention
-          to your party planning needs with each event customized to fit your criteria.</h3>
+          to your party planning needs with each event customized to fit your criteria.
+          </h1>
         </div>
-
+        <div className="PrivateEventsPhotos">
+          <article className="PrivateEventsPhoto1">
+          </article>
+          <article className="PrivateEventsPhoto2">
+          </article>
+        </div>
+        <div className="PrivateEventsPhotos">
+          <article className="PrivateEventsPhoto3">
+          </article>
+          <article className="PrivateEventsPhoto4">
+          </article>
+        </div>
         <div className="RoomDescription">
-        <a href="mailto:pocopiattiparty@gmail.com" className="cta">Email Us</a>
+        <a href="mailto:pocopiattiparty@gmail.com"><button>Email Us</button></a>
         <h1>Or call 419.931.0281</h1>
-
+          <PerrysburgRoom />
+          <DowntownRoom />
         </div>
-      </div>
-      <div className="private-events-content">
-        <div className="event-location wrap">
-          <div className="event-location-desc">
-            <PerrysburgRoom />
-          </div>
-        </div>
-        <div className="event-location wrap">
-          <div className="event-location-desc">
-            <DowntownRoom />
-          </div>
-        </div>
-
 
       </div>
-      <PrimaryCallout />
     </div>
   </Layout>
 )
